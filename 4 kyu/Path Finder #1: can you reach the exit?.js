37ee6284992024072a63ed42e5bb470459af52bd
Ali El Moussaui Rami <ali.em@omc.com>
2026-01-08 16:42:31 +0100

    4 kyu: Path Finder #1: can you reach the exit?

---
+++ 4 kyu/Path Finder #1: can you reach the exit?.js
@@ -0,0 +1,53 @@
+// https://www.codewars.com/kata/5765870e190b1472ec0022a2
+
+function pathFinder(maze) {
+    const grid = maze.trim().split("\n").map((row) => row.split(""));
+    const size = grid.length;
+    const startKey = "0,0"
+
+    const visited = new Set();
+    const queue = [{x: 0, y: 0, key: startKey}]
+
+    let index = 0;
+
+    while(index < queue.length) {
+        const current = queue[index++];
+        
+        if(visited.has(current.key))
+            continue;
+
+        visited.add(current.key);
+
+        if(current.x === size - 1 && current.y === size - 1)
+            return true;
+
+        const directions = [
+            [0, -1],  // N
+            [0,  1],  // S
+            [1,  0],  // E
+            [-1, 0]   // W
+        ];
+
+        for ([dx, dy] of directions) {
+            const neighborX = current.x + dx;
+            const neighborY = current.y + dy;
+
+            if(neighborX < 0 || neighborY < 0 || neighborX >= size || neighborY >= size || grid[neighborY][neighborX] === "W")
+                continue
+
+            const neighborKey = `${neighborX},${neighborY}`;
+            if (visited.has(neighborKey))
+                continue;
+
+            queue.push({ x: neighborX, y: neighborY, key: neighborKey });
+        }
+
+    }
+
+    return false;
+}
+
+const maze = `..W
+.W.`;
+
+console.log(pathFinder(maze));
